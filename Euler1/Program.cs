3207d833953c8ed35b0fffd81d69454e60c11063
Milan Koster <m.kostervangroos@student.fontys.nl>
2020-12-23 17:04:48 +0100

    Merged existing solutions

---
+++ Euler1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Euler
+{
+    static class Program
+    {
+        public static void Main(string[] args)
+        {
+            int total = 0;
+            for (int count = 0; count < 1000; count++)
+            {
+                if (count % 3 == 0 || count % 5 == 0)
+                    total += count;
+            }
+            Console.WriteLine("Total " + total);
+        }
+    }
+}
